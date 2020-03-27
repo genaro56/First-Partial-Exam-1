6d9ff8b31225349493b42dba1ce2595eba9367d5
genaromartinez <genaro.mtz1998@gmail.com>
2020-03-27 08:55:50 -0600

    challenge 3

---
+++ Coding-Challenge-3/js/index.js
@@ -0,0 +1,24 @@
+window.onload = function() {
+    document.getElementById('submitBtn').addEventListener('click', function(event) {
+        event.preventDefault();
+        var inputNumber = document.querySelector('.fizzBuzzNumber').value
+        console.log(inputNumber)
+
+        const getNumber = (i) => {
+            if(i % 5 == 0) return 'buzz' 
+            if(i % 3 == 0) {
+                return 'fizz' 
+            }
+
+            return i;
+        }
+        for(let i = 0; i < inputNumber; i++) {
+            const html = `
+                <div class="square">
+                 <label>${getNumber(i)}</label>
+                </div>
+                `
+            document.getElementById('formSquare').insertAdjacentHTML('afterend', html)
+        }
+    })
+}
